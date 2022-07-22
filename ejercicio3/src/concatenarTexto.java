6ab5da42e365f3de387c9139f721ea8901e22562
ronsnaider <109690769+ronsnaider@users.noreply.github.com>
2022-07-22 15:59:48 +0200

    Add files via upload

---
+++ ejercicio3/src/concatenarTexto.java
@@ -0,0 +1,10 @@
+public class concatenarTexto {
+
+
+    public static void main(String[] args) {
+        String[] nombres = {"pepe", "juan", "maria"};
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
+        }
+    }
+}
